0b4858a7e23fd7bc4583ffb17e9d520b528a1d24
Netzer7 <58796038+Netzer7@users.noreply.github.com>
2022-09-25 21:29:22 -0700

    Add Twitter Part 1

---
+++ Week 3/Twitter/Twitter/TweetCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by user227032 on 9/25/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
