88119dadf3330faefa788da5f50b141685b56cd4
Netzer7 <58796038+Netzer7@users.noreply.github.com>
2022-10-05 10:56:43 -0700

    Add Tweet Segue
    Before we handle changes to API which broke last time

---
+++ Week 3/Twitter/Twitter/TweetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by user227032 on 10/3/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
